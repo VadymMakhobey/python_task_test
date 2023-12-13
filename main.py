6022cb90288a8dbb851d2a625a7b60e9d07df58d
new <vadimmakhobey@gmail.com>
2023-12-13 16:14:09 +0300

    Task 3

--- main.py
+++ main.py
@@ -39,5 +39,32 @@ def Plants_vs_Zombies():
     print(battle_result([1, 3, 5, 7], [2, 4, 0, 8]))  # True
     print(battle_result([2, 1, 1, 1], [1, 2, 1, 1]))  # True
 
+def Schedule_generator():
+    def generate_schedule(days, work_days, rest_days, start_date):
+        schedule = []
+        current_date = start_date
 
+        for _ in range(days):
+            if current_date.weekday() < 5:  # Перевірка, чи це робочий день (понеділок - п'ятниця)
+                schedule.append(current_date)
+                work_days -= 1
+            else:
+                rest_days -= 1
+
+            current_date += timedelta(days=1)
+
+            if work_days == 0 and rest_days == 0:
+                break
+
+        return schedule
+
+    # Параметри тесту
+    days = 5
+    work_days = 2
+    rest_days = 1
+    start_date = datetime(2020, 1, 30)
+
+    # Тест
+    result_schedule = generate_schedule(days, work_days, rest_days, start_date)
+    print(result_schedule)
 
